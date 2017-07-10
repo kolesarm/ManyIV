7be859a6e5d4b0227ce96a13971f0b0160997dcb
Michal Kolesár <kolesarmi@googlemail.com>
2017-07-10 17:44:58 -0400

    added inst/doc/ to git

---
+++ inst/doc/ManyIV.R
@@ -0,0 +1,20 @@
+## ---- include=FALSE, cache=FALSE-----------------------------------------
+library("knitr")
+knitr::opts_knit$set(self.contained = FALSE)
+knitr::opts_chunk$set(tidy = TRUE, collapse=TRUE, comment = "#>",
+                      tidy.opts=list(blank=FALSE, width.cutoff=55))
+
+## ------------------------------------------------------------------------
+library("ManyIV")
+## Specification as in Table V, columns (1) and (2) in Angrist and Krueger
+IVreg(lwage~education+as.factor(yob)|as.factor(qob)*as.factor(yob),
+            data=ak80, inference=c("standard", "re", "il", "lil"))
+
+## ------------------------------------------------------------------------
+r1 <- IVreg(lwage~education+as.factor(yob)|as.factor(qob)*as.factor(yob),
+            data=ak80, inference="md", approx=TRUE)
+print(r1, digits=4)
+
+## ------------------------------------------------------------------------
+IVoverid(r1)
+
